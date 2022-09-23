037d7543f90aa5ffcec390b0bb042efe76ab8f02
Felipe Andrade <feliperufini01@gmail.com>
2022-09-22 21:38:52 -0400

    alter: get profile info

--- pages/api/user.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { validateTokenJWT } from "../../middlewares/validateTokenJWT"
-
-const userEndpoint = (req : NextApiRequest, res : NextApiResponse) => {
-  return res.status(200).json('Usuário autenticado com sucesso!');
-}
-
-export default validateTokenJWT(userEndpoint);
