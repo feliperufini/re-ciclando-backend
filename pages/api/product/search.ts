46b125229d50c4e0adecbc2114ddd63693c45a03
Felipe Andrade <feliperufini01@gmail.com>
2022-10-17 23:05:54 -0400

    [feat] - search product

---
+++ pages/api/product/search.ts
@@ -0,0 +1,34 @@
+import type { NextApiResponse, NextApiRequest } from "next";
+import type { ResponseDefaultMsg } from "../../../types/ResponseDefaultMsg";
+import { connectMongoDB } from "../../../middlewares/connectMongoDB";
+import { validateTokenJWT } from "../../../middlewares/validateTokenJWT";
+import { ProductModel } from "../../../models/ProductModel";
+
+const searchProductEndpoint = async (req: NextApiRequest, res: NextApiResponse<ResponseDefaultMsg | any[]>) => {
+  try {
+    if (req.method !== 'GET') {
+      return res.status(400).json({ error: 'Método informado é inválido!' });
+    }
+
+    const { filter } = req.query;
+
+    if (!filter || filter.length < 2) {
+      return res.status(400).json({ error: 'Informe pelo menos dois caracteres na busca!' });
+    }
+
+    const productsSearch = await ProductModel.find({
+      $or: [
+        {name: {$regex : filter, $options: 'i'}},
+        {description: {$regex : filter, $options: 'i'}}
+      ]
+    });
+
+    return res.status(200).json(productsSearch);
+
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ error: 'Não foi possível buscar o produto!' });
+  }
+}
+
+export default validateTokenJWT(connectMongoDB(searchProductEndpoint));
